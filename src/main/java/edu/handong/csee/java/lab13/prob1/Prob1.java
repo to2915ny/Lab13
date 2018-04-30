795f969ddadd63c5fe4fb54cba2701d7304d2482
to2915ny <strikerlee95@gmail.com>
2018-04-30 15:44:17 +0900

    Changed prob1 main class to prob1

--- src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob1;
 
-public class Main {//public class Main
+public class Prob1 {//public class Prob1
 
 	public static void main(String[] args) {//public method main
 		
